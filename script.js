875646f43a34ed4e29b05ecf8c50b42169f7d659
Tahir Dincer <coskun.dincer2014@gmail.com>
2025-02-07 02:51:33 +0300

    ' Final code ' ' For now, better cards will be implemented, code will be refactored if can '

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const pokemonContainer = document.querySelector(".pokemon__container");
 async function handlePokemon(data) {
   const pokemonCard = document.createElement("div");
   pokemonCard.classList.add("pokemon__card");
+  const name = data.name.slice(0, 1).toUpperCase() + data.name.slice(1);
   pokemonCard.innerHTML = `<div class= "pokemon__card">
-        <h2> ${data.name} </h2>
-    
+        <h2> ${name} </h2>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png" alt= ${name}/>
         </div>
      `;
   pokemonContainer.appendChild(pokemonCard);
 }
 async function fetchPokemonsInOrder() {
-  for (let i = 1; i < pokemonQuantity; i++) {
+  for (let i = 1; i <= pokemonQuantity; i++) {
     await fetchPokemon(i);
   }
 }
@@ -24,24 +25,8 @@ async function fetchPokemon(id) {
 }
 
 fetchPokemonsInOrder();
-/*
-      //
-const pokemonContainer = document.querySelector("pokemon__container");
-const pokemonCard = document.createElement("div");
-pokemonCard.classList.add("pokemon__card");
-pokemonContainer.appendChild(pokemonCard);
 
-// For loop
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
+// So in the ' outer ' scope there is a ' fetchPokemonsInOrder ' function. This is why because ' it ' fetches pokemons ' one ' by ' one '
+// So there is a ' handlePokemon ' function inside ' fetchPokemon ' function. This is because in each iteration a div which has a class of 'pokemon__card' is ' iniated '
 
-pokemonCard.innerHTML= `<div class="pokemon__card">
-        <h2>Dikachu</h2>
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fassets.stickpng.com%2Fimages%2F580b57fcd9996e24bc43c325.png&f=1&nofb=1&ipt=736def7ece618c09f874f4424fc2a783005da849c234960e9ebcf7446304a99d&ipo=images"
-          alt="Pikachu"
-        />
-      </div>`
-      */
+// This cards will be implemented if I can ' in future ' https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs3-alpha.figma.com%2Fhub%2Ffile%2F1837652003%2F1954fce0-71ec-4ec8-8594-4da04c9e89b3-cover.png&f=1&nofb=1&ipt=b0070cbd53ef5eb1fa413cf30283a999a1498d1e093ef8699e5be042f8909633&ipo=images
